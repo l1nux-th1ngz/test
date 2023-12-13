ec82f1d11020275959e141d774b83c5e7c011547
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-12-13 05:29:38 -0700

    Update wp.sh

--- wp.sh
+++ wp.sh
@@ -1,11 +1,24 @@
 #!/bin/zsh
-swaybg --image $(find $HOME/Pictures/Wallpaper -type f | shuf -n 1) -m fill&
+background=$(find $HOME/Pictures/Wallpaper -type f | shuf -n 1)
+swaybg --image $background -m fill&
+
 function handle {
     if [[ ${1:0:9} == "workspace" ]]; then
         echo $line
-        killall swaybg
-        swaybg --image $(find $HOME/Pictures/Wallpaper -type f | shuf -n 1) -m fill&
+    newbackground=$(find $HOME/Pictures/Wallpaper -type f | shuf -n 1)
+    echo $newbackground
+    until [[ ("${background}" != "${newbackground}") && (-r $newbackground) ]]; do
+        newbackground=$(find $HOME/Pictures/Wallpaper -type f | shuf -n 1)
+        echo $newbackground
+        sleep .3
+        done
+    echo $newbackground
+    background=newbackground
+    swpid=`ps axf | grep swaybg | grep -v grep | awk '{printf $1}'`
+    swaybg --image $newbackground -m fill&
+    sleep .3
+    kill $swpid
     fi
 }
 
-socat - UNIX-CONNECT:/tmp/hypr/.socket2.sock | while read line; do handle $line; done
+socat - UNIX-CONNECT:/tmp/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock | while read line; do handle $line; done
