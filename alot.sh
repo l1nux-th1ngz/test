efe5fd89888010e249b36ac91a69cb7da0b8dd7b
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-12-13 09:15:53 -0700

    Update alot.sh

--- alot.sh
+++ alot.sh
@@ -1,63 +1,187 @@
 #!/bin/bash
 
-# Check if yay is installed
-if ! command -v yay &> /dev/null; then
-    # Install yay if not already installed
-    pacman -S --noconfirm yay
-fi
+# pacman installs these
+pacman -S --noconfirm git base-devel multilib-devel go rustup xdg-user-dirs
 
-# Check if paru is installed
-if ! command -v paru &> /dev/null; then
-    # Install paru if not already installed
-    yay -S --noconfirm paru
-fi
+# Prompt user to choose default rustup version
+echo "Choose default Rust version using 'rustup default <version>' (e.g., stable, nightly)"
+read -t 8 -p "Enter Rust version (timeout in 8 seconds): " rust_version
+rustup default "$rust_version" || { echo "Failed to set default Rust version"; sleep 8; }
 
-# Prompt user to choose between yay and paru
-echo "Choose AUR helper (1 for yay, 2 for paru):"
-read -r choice
+# Update xdg-user-dirs
+xdg-user-dirs-update
 
-case "$choice" in
-    1)
-        aur_helper="yay"
-        ;;
-    2)
-        aur_helper="paru"
-        ;;
-    *)
-        echo "Invalid choice. Defaulting to yay."
-        aur_helper="yay"
-        ;;
-esac
+# Check but do not exit this script ever
+git clone https://aur.archlinux.org/aurutils.git && cd aurutils && makepkg -si 
+git clone https://aur.archlinux.org/pacaur.git && cd pacaur && makepkg -si
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+# Install these with yay
+packages=(
+    "autoconf"
+    "autoconf-archive"
+    "automake"
+    "starship"
+    "wlroots"
+    "wayland"
+    "wayland-utils"
+    "wayland-protocols"
+    "gdb"
+    "ninja"
+    "gcc"
+    "cmake"
+    "meson"
+    "libxcb"
+    "xcb-proto"
+    "xcb-util"
+    "xcb-util-keysyms"
+    "libxfixes"
+    "libx11"
+    "libxcomposite"
+    "xorg-xinput"
+    "libxrender"
+    "pixman"
+    "wayland-protocols"
+    "cairo"
+    "pango"
+    "seatd"
+    "libxkbcommon"
+    "xcb-util-wm"
+    "xorg-xwayland"
+    "libinput"
+    "libliftoff"
+    "libdisplay-info"
+    "cpio"
+    "npm"
+    "jo"
+    "jq"
+    "tomlplusplus"
+    "clang"
+    "gcc"
+    "qt5-wayland"
+    "qt6-wayland"
+    "xdg-desktop-portal"
+    "xdg-desktop-portal-gtk"
+    "xdg-desktop-portal-wlr"
+    "nodejs"
+    "xdg-desktop-portal-hyprland"
+    "pipewire"
+    "wireplumber"
+    "dunst"
+    "python"
+    "python-pywal"
+    "strawberry"
+    "brightnessctl"
+    "bluez"
+    "bluez-libs"
+    "rofi-lbonn-wayland-git"
+    "networkmanager"
+    "network-manager-applet"
+    "wofi"
+    "qt5-gsettings"
+    "ffmpegthumbs"
+    "playerctl"
+    "lightly-qt"
+    "kvantum"
+    "polkit-kde-agent"
+    "ttf-font-awesome-5"
+    "jq"
+    "gufw"
+    "qt5ct"
+    "tar"
+    "gammastep"
+    "wl-clipboard"
+    "nwg-look-bin"
+    "thunderbird"
+    "visual-studio-code-bin"
+    "firefox"
+    "easyeffects"
+    "mako"
+    "hyprpicker"
+    "hyprshot-git"
+    "bc"
+    "sysstat"
+    "kitty"
+    "alacritty"
+    "sassc"
+    "zip"
+    "unzip"
+    "systemsettings"
+    "ttf-font-awesome-5"
+    "orchis-theme-git"
+    "acpi"
+    "fish"
+    "nwg-look"
+    "blueman"
+    "lightly-qt"
+    "firefox"
+    "mpc"
+    "mpd"
+    "vlc"
+    "mpv"
+    "celluloid"
+    "geany"
+    "geany-plugins"
+    "notepadqq"
+    "nm-connection-editor"
+    "glib2"
+    "gtk3"
+    "deadd-notification-center-bin"
+    "cava"
+    "thunar"
+    "ffmpeg"
+    "thunar-volman"
+    "thunar-archive-plugin"
+    "gvfs"
+    "ranger"
+    "swww"
+    "cliphist"
+    "copyq"
+    "spotify"
+    "spotify-adblock-git"
+    "spotify-wayland"
+    "tumbler"
+    "hyprland-git"
+    "aylurs-gtk-shell"
+    "eww-wayland"
+    "zenity"
+    "aalib"
+    "jp2a"
+    "ascii"
+    "i2pd"
+    "lsd"
+    "thefuck"
+    "archinstall"
+    "shell-color-scripts"
+    "udisks2"
+    "udiskie"
+    "aurutils"
+    "pavucontrol"
+    "xdg-user-dirs"
+    "pacman-contrib"
+    "reflector"
+    "python-pip"
+    "python-pipx"
+    "tk"
+    "python-connect"
+    "python-pyaml"
+    "python-click"
+    "yad"
+    "aconfmgr-git"
+    "xdg-base-dir-env"
+    "xdg-su"
+    "sudo"
+    "xdg-environment"
+    "gtk4-layer-shell"
+)
 
 # Install other essential packages with the chosen AUR helper
-$aur_helper -S --noconfirm autoconf autoconf-archive automake starship \
-  wlroots wayland wayland-utils wayland-protocols gdb ninja gcc cmake meson \
-  libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite \
-  xorg-xinput libxrender pixman wayland-protocols cairo pango seatd \
-  libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info \
-  cpio npm jo jq tomlplusplus clang gcc qt5-wayland qt6-wayland xdg-desktop-portal \
-  xdg-desktop-portal-gtk xdg-desktop-portal-wlr nodejs xdg-desktop-portal-hyprland \
-  pipewire wireplumber dunst python python-pywal strawberry brightnessctl bluez \
-  bluez-libs rofi-lbonn-wayland-git networkmanager network-manager-applet wofi \
-  qt5-gsettings ffmpegthumbs playerctl lightly-qt kvantum polkit-kde-agent \
-  ttf-font-awesome-5 jq gufw qt5ct tar gammastep wl-clipboard nwg-look-bin \
-  thunderbird visual-studio-code-bin firefox easyeffects mako hyprpicker \
-  hyprshot-git bc sysstat kitty alacritty sassc zip unzip systemsettings \
-  ttf-font-awesome-5 orchis-theme-git acpi fish nwg-look blueman lightly-qt \
-  firefox mpc mpd vlc mpv celluloid geany geany-plugins notepadqq \
-  nm-connection-editor glib2 gtk3 deadd-notification-center-bin cava thunar \
-  ffmpeg thunar-volman thunar-archive-plugin gvfs ranger swww cliphist copyq \
-  spotify spotify-adblock-git spotify-wayland tumbler hyprland-git \
-  aylurs-gtk-shell eww-wayland zenity aalib jp2a ascii i2pd lsd thefuck \
-  archinstall shell-color-scripts udisks2 udiskie aurutils pavucontrol \
-  xdg-user-dirs pacman-contrib reflector python-pip python-pipx tk python-connect \
-  python-pyaml python-click yad aconfmgr-git xdg-base-dir-env xdg-su sudo xdg-environment \
-  gtk4-layer-shell
-
-# Continue with the rest of the script...
+for package in "${packages[@]}"; do
+    yay -S --noconfirm "$package" || { echo "Failed to install $package"; exit 1; }
+done
 
 # Start paccache.timer
-sudo systemctl enable paccache.timer
+systemctl --user enable paccache.timer
 
 # Clone git repo
 git clone https://github.com/l1nux-th1ngz/colors.git
@@ -77,8 +201,8 @@ chmod +x ~/.config/hypr/scripts/*
 chmod +x ~/.config/ags/scripts/*
 
 # Setup environment
-sudo cp /etc/environment /etc/environmentOLD
-echo 'QT_QPA_PLATFORMTHEME=qt5ct' | sudo tee -a /etc/environment
+cp /etc/environment /etc/environmentOLD
+echo 'QT_QPA_PLATFORMTHEME=qt5ct' | tee -a /etc/environment
 
 # Copy theme files
 mkdir -p ~/.local/share/color-schemes/
@@ -95,16 +219,16 @@ mkdir -p ~/.themes
 tar xvf ~/.config/hypr/configs/gtk-themes/*.tar.gz -C ~/.themes
 
 # Install plymouth
-$aur_helper -S --noconfirm plymouth-theme-sweet-arch-git
+yay -S --noconfirm plymouth-theme-sweet-arch-git
 
 # Enable plymouth
-sudo plymouth-set-default-theme -R sweet-arch
+plymouth-set-default-theme -R sweet-arch
 
 # Clone, install, and enable sddm
 git clone https://github.com/stuomas/delicious-sddm-theme.git && cd delicious-sddm-theme && chmod +x ./install.sh
 
 # Enable sddm
-sudo systemctl enable sddm
+systemctl enable sddm
 
 # Update Arch
 sudo pacman -Syu --noconfirm
