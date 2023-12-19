420af775d863e803a8d0df7c87dc50ea1b37b624
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-12-19 08:53:13 -0700

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# Specify the path to your pacman.conf file
 pacman_conf="/etc/pacman.conf"
 
-# Backup the original pacman.conf file
-cp "$pacman_conf" "$pacman_conf.bak"
-
-# Uncomment and modify specific lines in pacman.conf
 sed -i 's/^#Color/Color/' "$pacman_conf"
 sed -i 's/^#ILoveCandy/ILoveCandy/' "$pacman_conf"
 sed -i 's/^#CheckSpace/CheckSpace/' "$pacman_conf"
@@ -16,4 +11,4 @@ sed -i 's/^#LocalFileSigLevel = Optional/LocalFileSigLevel = Optional/' "$pacman
 
 # You can add more customizations as needed
 
-echo "Customization of $pacman_conf completed. Backup saved as $pacman_conf.bak"
+echo "Customization of $pacman_conf completed."
